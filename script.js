cd8332771079b22dd82e8bd966d8bf6f2802ddf5
Joseph <limjoseph2004@gmail.com>
2023-12-02 19:00:24 +1100

    Add placeholder for input fields

---
+++ script.js
@@ -0,0 +1,5 @@
+const pw = document.querySelector("#pw").value;
+const pw2 = document.querySelector("#pw2").value;
+const submit = document.querySelector(".login");
+
+submit.addEventListener("click", () => {});
