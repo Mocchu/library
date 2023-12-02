ef2bc22efe3a1f950d69cb55e1ce34f6e346f591
Joseph <limjoseph2004@gmail.com>
2023-12-02 19:37:27 +1100

    Add error handling for matching passwords

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
-const pw = document.querySelector("#pw").value;
-const pw2 = document.querySelector("#pw2").value;
+const formList = document.querySelector(".form-list");
+const pw = document.querySelector("#pw");
+const pw2 = document.querySelector("#pw2");
 const submit = document.querySelector(".login");
 
-submit.addEventListener("click", () => {});
+let messageExists = false;
+
+pw2.addEventListener("input", () => {
+	if (pw.value != pw2.value) {
+		pw2.setCustomValidity("Passwords do not match!");
+		console.log("Passwords do not match!");
+		if (!messageExists) {
+			messageExists = true;
+			appendErrorMessage();
+		}
+	} else if (pw.value === pw2.value) {
+		pw2.setCustomValidity("");
+		console.log("Passwords match!");
+		if (messageExists) {
+			messageExists = false;
+			errorList.remove();
+		}
+	}
+});
+
+function appendErrorMessage() {
+	errorList = document.createElement("li");
+	errorMsg = document.createElement("p");
+	errorMsg.textContent = "Passwords do not match!";
+	errorMsg.style.color = "red";
+	errorMsg.style.fontSize = "0.9rem";
+	errorList.append(errorMsg);
+	formList.append(errorList);
+}
